f6cc0b99671fcd3bc472ff39c70ec9e3eb8d43d7
AtillaGoguslu <atillagoguslu@hotmail.com>
2024-12-29 18:06:53 +0300

    Görev 1 bitmiştir

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Profile from "./components/Profile.jsx";
 
-const Product = () => {
-  return (
-    <div>
-      <h2>Cookies</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
 };
 
 function App() {
   return (
-    <div>
-      <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
   );
 }
 
